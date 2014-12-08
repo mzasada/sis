011f767b43912fb56696bc947756bba1148c441a
mzasada <maciejekz@gmail.com>
2014-12-08 14:56:31 +1100

    Add explicit connect+read timeouts to async HTTP

--- src/main/java/org/sis/connector/ConnectorConfiguration.java
+++ src/main/java/org/sis/connector/ConnectorConfiguration.java
@@ -3,18 +3,31 @@ package org.sis.connector;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.AsyncClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
 import org.springframework.web.client.AsyncRestTemplate;
-
-import java.util.concurrent.ExecutorService;
 
 @Configuration
 public class ConnectorConfiguration {
 
+  private static final int HTTP_READ_TIMEOUT_IN_MILLIS = 2_000;
+  private static final int HTTP_CONNECT_TIMEOUT_IN_MILLIS = 2_000;
+
   @Bean
-  public AsyncRestTemplate asyncRestTemplate() {
-    return new AsyncRestTemplate();
+  @Autowired
+  public AsyncRestTemplate asyncRestTemplate(AsyncClientHttpRequestFactory asyncClientHttpRequestFactory) {
+    return new AsyncRestTemplate(asyncClientHttpRequestFactory);
+  }
+
+  @Bean
+  public AsyncClientHttpRequestFactory asyncClientHttpRequestFactory() {
+    HttpComponentsAsyncClientHttpRequestFactory factory = new HttpComponentsAsyncClientHttpRequestFactory();
+    factory.setReadTimeout(HTTP_READ_TIMEOUT_IN_MILLIS);
+    factory.setConnectTimeout(HTTP_CONNECT_TIMEOUT_IN_MILLIS);
+    return factory;
   }
 
   @Bean(initMethod = "start", destroyMethod = "close")
