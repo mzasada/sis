e32e4222584f037cdc4be9856b1d1f9e01068a13
mzasada <maciejekz@gmail.com>
2014-12-18 18:08:13 +1100

    Add finding collection config by name

--- src/main/java/org/sis/connector/solr/api/SolrDocumentServiceImpl.java
+++ src/main/java/org/sis/connector/solr/api/SolrDocumentServiceImpl.java
@@ -2,9 +2,12 @@ package org.sis.connector.solr.api;
 
 import org.json.simple.JSONObject;
 import org.sis.connector.solr.cluster.ClusterState;
+import org.sis.connector.solr.cluster.CollectionConfig;
+import org.sis.connector.solr.cluster.SolrNode;
 import org.sis.repl.bindings.operations.SaveDocument;
 import org.springframework.web.client.AsyncRestOperations;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 public class SolrDocumentServiceImpl implements SolrDocumentService {
@@ -19,6 +22,12 @@ public class SolrDocumentServiceImpl implements SolrDocumentService {
 
   @Override
   public CompletableFuture<JSONObject> save(SaveDocument operation) {
+    Optional<CollectionConfig> collectionConfig = clusterState.findCollectionConfigByName(operation.getCollectionName());
+
+    Optional<SolrNode> leaderNode = clusterState.findLeaderNodeForCollection(operation.getCollectionName());
+    operation.getCollectionName();
+
+    asyncRestOperations.getForEntity();
 
     return null;
   }
--- src/main/java/org/sis/repl/bindings/operations/SaveDocument.java
+++ src/main/java/org/sis/repl/bindings/operations/SaveDocument.java
@@ -3,14 +3,15 @@ package org.sis.repl.bindings.operations;
 import org.json.simple.JSONObject;
 import org.sis.connector.OperationExecutor;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class SaveDocument implements Operation {
 
   private final String collectionName;
-  private final JSONObject document;
+  private final Map<String, Object> document;
 
-  public SaveDocument(String collectionName, JSONObject document) {
+  public SaveDocument(String collectionName, Map<String, Object> document) {
     this.collectionName = collectionName;
     this.document = document;
   }
@@ -22,5 +23,9 @@ public class SaveDocument implements Operation {
 
   public String getCollectionName() {
     return collectionName;
+  }
+
+  public Map<String, Object> getDocument() {
+    return document;
   }
 }
